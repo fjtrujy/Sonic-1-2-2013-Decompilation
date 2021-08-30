bfe4c89efe8465810e70376f87e81c9c11154b05
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2021-08-31 00:38:08 +0200

    Adding IRX drivers to PS2

--- RSDKv4/drivers/engine/engine_PS2.cpp
+++ RSDKv4/drivers/engine/engine_PS2.cpp
@@ -1,6 +1,96 @@
 #include "../../RetroEngine.hpp"
 
-static void ps2_init() {}
+#if RETRO_PLATFORM == RETRO_PS2
+
+#include <kernel.h>
+#include <unistd.h>
+
+#include <sbv_patches.h>
+#include <sifrpc.h>
+#include <iopcontrol.h>
+#include <loadfile.h>
+#include <libmtap.h>
+#include <libpad.h>
+
+extern unsigned char sio2man_irx;
+extern unsigned int size_sio2man_irx;
+
+extern unsigned char iomanX_irx;
+extern unsigned int size_iomanX_irx;
+
+extern unsigned char fileXio_irx;
+extern unsigned int size_fileXio_irx;
+
+extern unsigned char usbd_irx;
+extern unsigned int size_usbd_irx;
+
+extern unsigned char bdm_irx;
+extern unsigned int size_bdm_irx;
+
+extern unsigned char bdmfs_vfat_irx;
+extern unsigned int size_bdmfs_vfat_irx;
+
+extern unsigned char usbmass_bd_irx;
+extern unsigned int size_usbmass_bd_irx;
+
+extern unsigned char mtapman_irx;
+extern unsigned int size_mtapman_irx;
+
+extern unsigned char padman_irx;
+extern unsigned int size_padman_irx;
+
+static void load_modules() 
+{
+   /* I/O Files */
+   SifExecModuleBuffer(&iomanX_irx, size_iomanX_irx, 0, NULL, NULL);
+   SifExecModuleBuffer(&fileXio_irx, size_fileXio_irx, 0, NULL, NULL);
+   SifExecModuleBuffer(&sio2man_irx, size_sio2man_irx, 0, NULL, NULL);
+
+   /* USB */
+   SifExecModuleBuffer(&usbd_irx, size_usbd_irx, 0, NULL, NULL);
+   SifExecModuleBuffer(&bdm_irx, size_bdm_irx, 0, NULL, NULL);
+   SifExecModuleBuffer(&bdmfs_vfat_irx, size_bdmfs_vfat_irx, 0, NULL, NULL);
+   SifExecModuleBuffer(&usbmass_bd_irx, size_usbmass_bd_irx, 0, NULL, NULL);
+
+    /* Controllers */
+   SifExecModuleBuffer(&mtapman_irx, size_mtapman_irx, 0, NULL, NULL);
+   SifExecModuleBuffer(&padman_irx, size_padman_irx, 0, NULL, NULL);
+
+   if (mtapInit() != 1)
+   {
+      printLog("mtapInit library not initalizated\n");
+   }
+   if (padInit(0) != 1)
+   {
+      printLog("padInit library not initalizated\n");
+   }
+}
+
+static void reset_IOP()
+{
+   SifInitRpc(0);
+#if !defined(DEBUG) || defined(BUILD_FOR_PCSX2)
+   /* Comment this line if you don't wanna debug the output */
+   while(!SifIopReset(NULL, 0)){};
+#endif
+
+   while(!SifIopSync()){};
+   SifInitRpc(0);
+   sbv_patch_enable_lmb();
+   sbv_patch_disable_prefix_check();
+}
+
+
+#endif
+
+static void ps2_init() {
+#if RETRO_PLATFORM == RETRO_PS2
+    reset_IOP();
+    load_modules();
+
+    sleep(5);
+#endif
+}
 static bool ps2_run() { return true; }
 static bool ps2_processEvent() { return true; }
 static void ps2_quit() {}
